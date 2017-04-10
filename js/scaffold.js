5a9b72949de563c5f44a639590e712779a4670d6
xiao <xiao@localhost>
2017-04-10 22:18:03 +0800

    a basic framework

---
+++ js/scaffold.js
@@ -0,0 +1,15 @@
+/**
+ * Created by xiao on 2017/4/10.
+ */
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
